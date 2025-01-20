fb92cd502947953dd560cbc0a12ae52e2115af17
jsbmomo <tytyes40@gmail.com>
2025-01-20 12:58:11 +0900

    H2 비밀번호 변경 및 더미 데이터 수정

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,22 +1,28 @@
-create sequence if not exists hibernate_sequence start with 1 increment by 1;
+-- Auto increment 초기화 및 증가를 수동으로 할 경우.
+-- create sequence if not exists hibernate_sequence start with 1 increment by 1;
+-- call next value for hibernate_sequence;
 
-call next value for hibernate_sequence;
 insert into member (name, age, address, email, reg_dt) values ('홍길동', 20, '대한민국 서울', 'hong@naver.com', now());
-
-call next value for hibernate_sequence;
 insert into member (name, age, address, email, reg_dt) values ('이흥선', 25, '대한민국 인천', 'lee@naver.com', now());
-
-call next value for hibernate_sequence;
 insert into member (name, age, address, email, reg_dt) values ('Mr.Kim', 21, '대한민국 광주', 'kim21@naver.com', now());
-
-call next value for hibernate_sequence;
 insert into member (name, age, address, email, reg_dt) values ('김규하', 28, '대한민국 울산', 'kim28@naver.com', now());
-
-call next value for hibernate_sequence;
 insert into member (name, age, address, email, reg_dt) values ('진민수', 30, '대한민국 부산', 'jin@naver.com', now());
 
-insert into product (prod_id, prod_name, prod_price) values (1, '노트북', 3200000);
-insert into product (prod_id, prod_name, prod_price) values (2, '모니터', 550000);
-insert into product (prod_id, prod_name, prod_price) values (3, '키보드', 189000);
-insert into product (prod_id, prod_name, prod_price) values (4, '마우스', 126000);
-insert into product (prod_id, prod_name, prod_price) values (5, '사무용품', 50000);
+insert into product (prod_name, prod_price) values ('노트북', 3200000);
+insert into product (prod_name, prod_price) values ('모니터', 550000);
+insert into product (prod_name, prod_price) values ('키보드', 189000);
+insert into product (prod_name, prod_price) values ('마우스', 126000);
+insert into product (prod_name, prod_price) values ('사무용품', 50000);
+
+insert into purchase_history (prod_id, user_id) values (1, 3);
+insert into purchase_history (prod_id, user_id) values (1, 4);
+insert into purchase_history (prod_id, user_id) values (2, 1);
+insert into purchase_history (prod_id, user_id) values (2, 2);
+insert into purchase_history (prod_id, user_id) values (2, 3);
+insert into purchase_history (prod_id, user_id) values (2, 4);
+insert into purchase_history (prod_id, user_id) values (3, 5);
+insert into purchase_history (prod_id, user_id) values (4, 1);
+insert into purchase_history (prod_id, user_id) values (4, 2);
+insert into purchase_history (prod_id, user_id) values (4, 5);
+insert into purchase_history (prod_id, user_id) values (5, 1);
+insert into purchase_history (prod_id, user_id) values (5, 5);
--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -2,20 +2,31 @@ DROP TABLE IF EXISTS member;
 
 CREATE TABLE member
 (
-    id      BIGINT          NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(30)     NOT NULL,
+    user_id BIGINT              NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(30)         NOT NULL,
     age     INT,
     address VARCHAR(255),
     email   VARCHAR(100),
-    reg_dt  DATETIME        NOT NULL,
-    PRIMARY KEY (id)
+    reg_dt  DATETIME            NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
 DROP TABLE IF EXISTS product;
 
 CREATE TABLE product
 (
-    prod_id     IDENTITY        PRIMARY KEY,
+    prod_id     BIGINT          NOT NULL AUTO_INCREMENT,
     prod_name   VARCHAR(255)    NOT NULL,
-    prod_price  INT             NOT NULL
+    prod_price  INT             NOT NULL,
+    PRIMARY KEY (prod_id)
+);
+
+DROP TABLE IF EXISTS purchase_history;
+
+CREATE TABLE purchase_history
+(
+    history_id  BIGINT         NOT NULL AUTO_INCREMENT,
+    prod_id     BIGINT,
+    user_id     BIGINT,
+    PRIMARY KEY (history_id)
 );
